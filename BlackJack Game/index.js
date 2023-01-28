c46f60d1337fde70b466452ca1619f5234fd32ec
Prashant Mishra <prashantmishra1100@gmail.com>
2023-01-28 14:42:28 +0530

    master

--- BlackJack Game/index.js
+++ BlackJack Game/index.js
@@ -1,33 +1,47 @@
 
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let sum = firstCard + secondCard
-let cards = [firstCard, secondCard] 
+let player = {
+    name: "Prashant",
+    chips: 200
+}
+let cards = [] 
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = " "
-
 let messageEl = document.getElementById("message-el")
 let sumEL = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
-    return 5
+    let randomNumber = Math.floor( Math.random() * 13 ) + 1  
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    } 
 }
 
 function startGame() {
-     renderGame
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
 }
 
 function renderGame() {
     cardsEl.textContent = "Cards: "  
-    for (let i = 0; i < cards.length ; i++) {
+    for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
     }
 
     sumEL.textContent ="Sum: " + sum
-
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -40,16 +54,17 @@ function renderGame() {
     messageEl.textContent = message
     // render out all the cards we have
     
-    // render out first and second card
-   
+    // render out first and second card  
 }
 
 function newCard() {
-    let card = 7
+    if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard()
     sum += card
     cards.push(card)
-    console.log(cards)
+    //console.log(cards)
     renderGame()
+    }   
 }
 
 // Array - ordered list of items - composite / complex data type 
@@ -87,15 +102,42 @@ function newCard() {
         return Math.floor( Math.random() * 6) + 1
     }
     console.log(rollDice())
-*/
 
+
+//  Practice on Objects
+   
+    let person = {
+        name: "Ram",
+        age: 24,
+        country: "India" 
+    }
+    function logData() {
+       console.log( person.name + " is " + person.age + " years old and lives in "
+       + person.country ) 
+    }
+    logData()
+ 
+//  Practice on Array and Loop
+    
+    let largeCountries = ["Chain","India","USA","Indonesia","Brazil"]
+    for (i = 0; i < largeCountries.length ; i ++) {
+        console.log("- " + largeCountries[i])
+    }
+
+//  Game
+    let hands = ["rock", "paper", "scissor"]
+    function getHand() {
+       let randomIndex = Math.floor( Math.random() * 3)
+       return hands[randomIndex]
+    } 
+    console.log( getHand()) 
+*/
 
 /*
 
 // Practice if-else
 
     let age = 102
-
     if (age < 100) {
         console.log("Not elegible")
     } else if(age == 100) {
