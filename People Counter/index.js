e1d5188fbbce69df1a779d005e39eac46156267e
Prashant Mishra <prashantmishra1100@gmail.com>
2023-01-31 20:38:33 +0530

    Added Decrement Button

--- People Counter/index.js
+++ People Counter/index.js
@@ -1,17 +1,20 @@
-
-
 let saveEl =document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
-
+// Increments the counter
 function increment() {
     count += 1 ;
     countEl.innerText = count;
-    
 }
 
+// Deccrements the counter
+function decrement() {
+    count -= 1 ;
+    countEl.innerText = count;
+}
 
+// Saves the counter
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
@@ -20,33 +23,3 @@ function save() {
     count = 0
 }
 
-/*
-
-let firstName = "Prashant"
-let lastName = "Mishra"
-let fullName = firstName + " " + lastName
-console.log(fullName)
-
-let name = "PM"
-let greeting = "Hi there"
-
-function greetPM() {
-    console.log(greetPM = greeting + "," + name + "!")    
-}
-greetPM()
-*/
-
-let myPoint = 3 
-
-function add3Point() {
-    myPoint += 3
-}
-
-function remove1Point() {
-    myPoint -= 1
-}
-add3Point()
-add3Point()
-add3Point()
-remove1Point()
-console.log(myPoint)
